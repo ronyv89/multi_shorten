41a4d5852a66dd867e1b4f6289bf95337d1bae59
Rony Varghese <ronyv250289@gmail.com>
2012-11-05 11:16:53 +0530

    Fixing typo in spec

--- spec/multi_shorten/client_spec.rb
+++ spec/multi_shorten/client_spec.rb
@@ -3,12 +3,12 @@ require 'multi_shorten/client'
 
 describe MultiShorten::Client do
 
-	before :each do 
+	before :each do
 		subject {MultiShorten::Client.new}
 	end
 
 	describe "#shorten" do
-		
+
 		it "should return error messages if required parameters are missing" do
 			expect { subject.shorten({}) }.to raise_error("Missing required parameter: url")
 			expect { subject.shorten({:url => "http://www.ggogle.com"}) }.to raise_error("Missing required parameter: mode")
@@ -30,7 +30,7 @@ describe MultiShorten::Client do
 		it "should return shortened urls for each of the specified apis" do
 			MultiShorten::B54.stub(:shorten).with("http://www.google.com").and_return({:status => :success, :short_url => "http://b54.in/9o"})
 			MultiShorten::QrCx.stub(:shorten).with("http://www.google.com").and_return({:status => :success, :short_url => "http://qr.cx/9o"})
-			subject.send(:multiple, ["b54", "qr_cx"], "http://www.google.com").should == { "qr_cx"=>{:status=>:success, :short_url=>"http://qr.cx/9o"}, "b54"=>{:status=>:success, :short_url=>"http://b54.in/9o}
+			subject.send(:multiple, ["b54", "qr_cx"], "http://www.google.com").should == { "qr_cx"=>{:status=>:success, :short_url=>"http://qr.cx/9o"}, "b54"=>{:status=>:success, :short_url=>"http://b54.in/9o"}}
 		end
 	end
 
